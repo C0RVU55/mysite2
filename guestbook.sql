0774b96cd5be7ad559bfcbbb3acb45bd632925b2
이다현 <74286308+C0RVU55@users.noreply.github.com>
2021-01-17 15:11:35 +0900

    mysite2_board

--- guestbook.sql
+++ guestbook.sql
@@ -26,7 +26,8 @@ SELECT  no,
         password,
         content,
         to_char(reg_date, 'YYYY-MM-DD HH24:MI:SS') reg_date
-FROM guestbook;
+FROM guestbook
+order by no desc;
 
 --입력(날짜는 컬럼명이 아니라 sysdate만 넣음. select는 어떻게 보여줄지를 나타내는 거) 
 insert into guestbook
